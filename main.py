d613c1f93db94d0261e5b82beb5ee67d30d9cf22
gabriel123491 <joaogabrielgamer78@gmail.com>
2026-07-31 19:51:36 -0300

    Add menu and functions for employee management

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-funcionarios = input("digite o nome do funcionario: ")
-print(f"o nome cadastrado foi: {funcionarios}")
+def apresenta_menu():
+    print("====================================")
+    print("     SISTEMA DIGESTÃO DE ESCALA     ")
+    print("====================================")
+    print("")
+    print("   1. Cadastrar funcionário 🙎")
+    print("   2. Listar funcionário 🙎🙎")
+    print("   0. Sair ❌")
+    print("")
+    opcao_menu = input("Escolha uma opção: ")
+    return opcao_menu
+    
+    
+
+def cadastrar_funcionario():
+    funcionarios = input("digite o nome do funcionario: ")
+    print(f"o nome cadastrado foi: {funcionarios}")
+    
+def listar_funcionario():
+    print("listando_funcionario")
+    
+def sair():
+    print("Saindo do sistema digestão de escala👋")
+    
+    
+#=====================================================
+    
+opcao_menu = apresenta_menu()
+
+
+match opcao_menu:
+        case "1":
+            cadastrar_funcionario() 
+        case "2":
+            listar_funcionario() 
+        case "0":
+            sair()
+        case _:  
+            print("Opção Inválida!")
