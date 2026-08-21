1325fe381ccb9636d09239218642c44012ce20f2
gabriel123491 <joaogabrielgamer78@gmail.com>
2026-08-21 19:56:16 -0300

    Implement file handling for employee registration
    Refactor the employee registration system to save data to a file and list registered employees.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+path_bd = Path("sincro-escala/BD") / "funcionario_bd.txt"
 funcionarios = []
 sair_do_sistema = False
 
@@ -12,25 +15,29 @@ def apresenta_menu():
     print("")
     opcao_menu = input("Escolha uma opção: ")
     return opcao_menu
-    
-    
 
 def cadastrar_funcionario():
-    funcionario = input("digite o nome do funcionario: ")
-    funcionarios.append(funcionario)
-    print(f"o nome cadastrado foi: {funcionario}")
-    print("====================================")
-    print("Você gostaria de adicionar um novo funcionário?")
-    print("   1. Sim ✅")
-    print("   2. Não ❌")
+    funcionário = input("digite o nome do funcionario: ")
+    with open(path_bd,"a", encoding="utf-8") as arquivo:
+        arquivo.write(f"{funcionário}\n")
+    funcionarios.append(funcionário)
+    print(f"O nome cadastrado foi: {funcionário}")
+    print("======================================")
+    print("você gostaria de adicionar um novo funcionário?")
+    print("1. Sim ✅")
+    print("2. Não ❌")
     seguir_cadastro = input("Escolha uma opção: ")
-    print("====================================")
-    if seguir_cadastro == "1":
-        cadastrar_funcionario() 
+    print("======================================")
+    if seguir_cadastro  == "1":    
+        cadastrar_funcionario()
     if seguir_cadastro == "2":
-        print("Cadastre concluído! ✅")              
-def listar_funcionario():
-    print("listando_funcionario")
+        print("cadastro concluido ✅")
+              
+def listar_funcionarios():
+    with open(path_bd,"r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            print(linha.strip())
+    
     
 def sair():
     print("Saindo do sistema digestão de escala👋")
@@ -45,8 +52,9 @@ while not sair_do_sistema:
             case "1":
                 cadastrar_funcionario() 
             case "2":
-                listar_funcionario() 
+                listar_funcionarios() 
             case "0":
                 sair()
+                break
             case _:  
                 print("Opção Inválida!")
